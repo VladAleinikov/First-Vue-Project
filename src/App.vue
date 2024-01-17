3d7586d96fc5c2744e278b92bdca50058c30a3a9
Vladik <vlad.a.aleynikov@gmail.com>
2024-01-17 11:54:44 +0300

    create vue project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>Likes = {{ likes }}</div>
+      <button @click="likes++">+</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+      data(){
+            return{
+                  likes: 0
+            }
+      }
+}
+</script>
+
+<style scoped></style>
