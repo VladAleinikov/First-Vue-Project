7a16b58164d4c63ef4ff6993410dd67ec56a51f7
Vladik <vlad.a.aleynikov@gmail.com>
2024-01-21 17:00:52 +0300

    add mixins

--- src/components/UI/MyDialog.vue
+++ src/components/UI/MyDialog.vue
@@ -7,19 +7,11 @@
 </template>
 
 <script>
+import toggleMixin from '../../mixins/toggleMixin';
+
 export default {
       name: "my-dialog",
-      props: {
-            show: {
-                  type: Boolean,
-                  default: false
-            }
-      },
-      methods: {
-            closeDialog(e) {
-                  this.$emit("update:show", false);
-            }
-      }
+      mixins: [toggleMixin]
 }
 </script>
 
